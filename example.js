6d4e9f0b91e4eefd9b7d62bc38eca81e208afa09
Laurel McLean <laurel.mclean@live.ca>
2023-09-27 12:35:17 -0600

    completed assignment

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 const { ordinalSuffix } = require('ordinal-suffix-lib');
 
 function printNumbers() {
-    for (let index = 0; index <= 100; index++) {
+    for (let index = 1; index <= 100; index++) {
         console.log(ordinalSuffix(index))
     }
 }
